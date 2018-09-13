ea351fc3dcd27c324a059f087d9c6defb3cd8e3e
Monferd Collin <mjc695@gmail.com>
2018-09-13 12:47:14 -0400

    halfway there!

--- src/selector.js
+++ src/selector.js
@@ -1,1 +1,25 @@
 // YOUR CODE HERE
+class $ {
+    constructor(selectorString){
+        // this.elements = [];
+        
+        if (selectorString[0] === '#'){
+            this.elements = [document.getElementById(selectorString.slice(1))];
+            console.log(selectorString.slice(1))
+        }
+        else if (selectorString[0] === '.'){
+            this.elements = Array.from(document.getElementsByClassName(selectorString.slice(1)));
+            console.log(selectorString.slice(1))
+        }
+        else {
+            this.elements = Array.from(document.getElementsByTagName(selectorString.toUpperCase()));
+        }
+    }
+
+    hide(){
+
+    }
+
+  
+}
+
